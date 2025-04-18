1238305ef0bae0f57ece8439d4a23d003cc321ac
Alexandre-Puech <alexandre.puech3@gmail.com>
2025-04-18 15:05:39 +0200

    Step9: feat: managed redirection for invalid id

--- src/pages/accomodation/index.jsx
+++ src/pages/accomodation/index.jsx
@@ -1,15 +1,20 @@
 import Slideshow from "../../components/Slideshow";
 import Logements from "../../data/logements.json";
-import { useParams } from "react-router";
+import { useParams, useNavigate } from "react-router";
 import "../../styles/css/Accomodation.css";
 import Logement from "../../components/Accomodation/index";
+import { useEffect } from "react";
 
 function Accomodation() {
   const { id } = useParams();
+  let navigate = useNavigate();
   const logement = Logements.find((logement) => logement.id === id);
-  if (!logement) {
-    return <div></div>;
-  }
+  useEffect(() => {
+    if (!logement) {
+      navigate("/*"), { replace: true };
+    }
+  }, [logement, navigate]);
+  if (!logement) return null;
   return (
     <div className="accomodation-body">
       <Slideshow pictures={logement.pictures} />
